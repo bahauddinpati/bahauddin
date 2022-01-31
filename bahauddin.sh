b330b3b385d493d666801a8c8e8cf6606cb3c9e1
bahauddinpati <98617936+bahauddinpati@users.noreply.github.com>
2022-02-01 03:11:04 +0700

    Create bahauddin.sh

---
+++ bahauddin.sh
@@ -0,0 +1,234 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+p='\033[37;1m' #putih
+echo
+echo
+python2 loginku.py
+echo
+clear
+echo
+echo $red "SELAMAT DATANG DI TOOLSKU (Bahauddin Tools Installer)"
+sleep 1
+echo
+echo
+echo
+
+echo
+clear
+toilet -f mono9 --filter metal Bahauddin
+
+echo
+echo $me "▬▬▬▬▬▬▬▬▬▬▬"$p"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo $i  "AUTHOR   : Bahauddin"
+echo $i  "FROM     : BENDOKATON ,PURWOKERTO TAYU PATI JATENG"
+echo $i  "WA       : 081330771837"
+echo $i  "YOUTUBE  : Bahauddin Channel/BahauddinStyle"
+echo $i  "TOOLS    : INSTALLER FOR HACKING"
+echo $i  "BLOG     : https://tutorialbahauddinstyle.my.id/"
+echo $me "▬▬▬▬▬▬▬▬▬▬▬"$p"▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"
+echo
+echo
+echo $cy"⎚▁▂▃▄▅ DAFTAR TOOLS HACKING MONGGO DI PILIH LUR ▆▅▄▃▂▁⎚"
+echo
+echo
+echo $ku"["$me"1"$ku"]"$i" METASPLOIT"
+echo $ku"["$me"2"$ku"]"$i" HACK FB (BRUTE PORCE)"
+echo $ku"["$me"3"$ku"]"$i" RED HAWK"
+echo $ku"["$me"4"$ku"]"$i" VIRUS (KANGGO GAWE VIRUS CUK)"
+echo $ku"["$me"5"$ku"]"$i" ADV PHISING (KANGGO PHISING LUR)"
+echo $ku"["$me"6"$ku"]"$i" WISHFISH (KANGGO PHISING)"
+echo $ku"["$me"7"$ku"]"$i" SQLMAP (SQL INJECTION)"
+echo $ku"["$me"8"$ku"]"$i" WEBPWN3R (KANGGO SCAN SQLI URL)"
+echo $ku"["$me"9"$ku"]"$i" Z PHISHER (KANGGO PHISING GAES)"
+echo $ku"["$me"10"$ku"]"$i" M DORK (KANGGO DORKING)"
+echo $ku"["$me"11"$ku"]"$i" D-TECT-1"
+echo $ku"["$me"12"$ku"]"$i" TM SCANNER"
+echo $ku"["$me"13"$ku"]"$i" ADMIN LOGIN (KANGGO GOLEK ADMIN LOGIN)"
+echo $i"["$me"14"$i"]"$ku" EXIT"
+echo
+echo $pur"❬✔❭Bahauddin❬✔❭"$pur"PATI CYBER TEAM"$pur"]"
+read -p"  ╼╼╼➤" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "METASPLOIT"|lolcat
+sleep 1
+pkg update && pkg upgrade -y
+pkg install wget -y
+pkg install curl -y
+pkg install git -y
+pkg install openssh -y
+cd $HOME
+wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
+chmod +x metasploit.sh
+bash metasploit.sh
+msfconsole
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "BRUTE FORCE"|lolcat
+sleep 1
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+bash new.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "RED HAWK"|lolcat
+sleep 1
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "VIRUS X"|lolcat
+sleep 1
+git clone https://github.com/TSMaitry/VirusX
+cd VirusX
+python2 VirusX.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "ADVPHISING"|lolcat
+sleep 1
+git clone https://github.com/Ignitetch/AdvPhishing.git
+cd AdvPhishing
+chmod +x *
+bash Android-Setup.sh
+bash AdvPhishing.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "WISHFISH"|lolcat
+sleep 1
+pkg install php -y
+pkg install wget -y
+pkg install openssh -y
+git clone https://github.com/kinghacker0/WishFish.git
+cd WishFish
+bash wishfish.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "SQLMAP"|lolcat
+sleep 1
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+chmod +x sqlmap.py
+python2 sqlmap.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "WEBPWN3R"|lolcat
+sleep 1
+git clone https://github.com/zigoo0/webpwn3r
+cd webpwn3r
+chmod +x scan.py
+python2 scan.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "Z PHISHER"|lolcat
+sleep 1
+pkg install git -y
+pkg install php -y
+pkg install openssh -y
+pkg install curl -y
+git clone https://github.com/htr-tech/zphisher
+cd zphisher
+chmod +x zphisher.sh
+bash zphisher.sh
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "M DORK"|lolcat
+sleep 1
+pkg install python2
+pip2 install mechanize
+pkg install git
+git clone https://github.com/Ranginang67/M-dork
+cd M-dork
+python2 mdork.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "D-TECT-1"|lolcat
+sleep 1
+git clone https://github.com/shawarkhanethicalhacker/D-TECT-1
+cd D-TECT-1
+python2 d-tect.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "TM SCANNER"|lolcat
+sleep 1
+git clone https://github.com/TechnicalMujeeb/TM-scanner
+cd TM-scanner
+sh install.sh
+python2 tmscanner.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "ADMIN LOGIN"|lolcat
+sleep 1
+git clone https://github.com/s0md3v/Breacher
+cd Breacher
+python2 breacher.py
+fi
+
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "METU LUR"|lolcat
+sleep 2
+echo $cy"MATURSUWUN WES NGANGGO TOOLS GAWENANKU"
+sleep 2
+echo $i"GUNAKAN DENGAN BIJAK DAN JANGAN DI SALAH GUNAKAN"
+sleep 2
+echo $pur"INTERNET LUAS LUR PELAJARILAH APA YG HARUS DI PELAJARI"
+sleep 2
+echo $ku"WA   :"$i" 081330771837  "
+echo $ku"YOUTUBE  :"$i" Bahauddin CHANNEL/BAHAUDDIN STYLE"
+echo $ku"BLOG/WEB :"$i" https://tutorialbahauddinstyle.my.id  "
+sleep 2
+echo $pur"DONT STUDY HARD BUT ENJOY YOUR STUDY"
+sleep 2
+echo $pur"CREATED BY : Bahauddin (PATI CYBER TEAM)"
+exit
+fi
